f9f9ec256580aaf96f471eac943f65cada35ebaf
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-09-19 18:26:28 +0200

    Terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let crearExcusa = function() {
+  //Sentencias
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //Recorrer con math.floor y randomizar las sentencias según las palabras que tenga cada array (length).
+  let IWho = Math.floor(Math.random() * who.length);
+  let IAction = Math.floor(Math.random() * action.length);
+  let IWhat = Math.floor(Math.random() * what.length);
+  let IWhen = Math.floor(Math.random() * when.length);
+
+  //Concatenar sentencias junto a su randomizador
+  return who[IWho] + "" + action[IAction] + "" + what[IWhat] + "" + when[IWhen];
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //Botón para ejecutar función
+  document.querySelector("#ejecutar").addEventListener("click", () => {
+    //Excusa random
+    document.querySelector("#excuse").innerHTML = crearExcusa();
+  });
 };
